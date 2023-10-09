1db49f851969ce3be1a95832a95b106b40668460
PantheraNestah <pantheranestah@gmail.com>
2023-10-09 08:17:23 +0300

    changed configurations

--- src/main/java/com/SpringBoot/clientsNotification/ClientsNotificationApplication.java
+++ src/main/java/com/SpringBoot/clientsNotification/ClientsNotificationApplication.java
@@ -1,31 +1,13 @@
 package com.SpringBoot.clientsNotification;
 
-import com.SpringBoot.clientsNotification.Utility.PropertiesConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientsNotificationApplication {
 
-	@Autowired
-	private PropertiesConfig propertiesConfig;
 	public static void main(String[] args) {
 		SpringApplication.run(ClientsNotificationApplication.class, args);
 	}
 
-	@Autowired
-	public void setPropertiesConfig()
-	{
-		propertiesConfig.setDbName(System.getenv("db_name"));
-		propertiesConfig.setDbPass(System.getenv("db_pass"));
-		propertiesConfig.setDbUrl(System.getenv("db_url"));
-
-		propertiesConfig.setMailName(System.getenv("mail_name"));
-		propertiesConfig.setMailPass(System.getenv("mail_pass"));
-		propertiesConfig.setMailHost(System.getenv("mail_host"));
-
-		propertiesConfig.setApiKey(System.getenv("at_key"));
-		propertiesConfig.setAtName(System.getenv("db_name"));
-	}
 }
