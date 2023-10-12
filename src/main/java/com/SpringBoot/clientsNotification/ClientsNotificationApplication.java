ce7b3ca70e5109a665801f16852bc0c5eecdaa37
PantheraNestah <pantheranestah@gmail.com>
2023-10-12 21:43:01 +0300

    written the exact jdbc url

--- src/main/java/com/SpringBoot/clientsNotification/ClientsNotificationApplication.java
+++ src/main/java/com/SpringBoot/clientsNotification/ClientsNotificationApplication.java
@@ -1,20 +1,16 @@
 package com.SpringBoot.clientsNotification;
 
-import com.SpringBoot.clientsNotification.Utility.CredConf;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientsNotificationApplication {
 
-	@Autowired
-	private CredConf credConf;
 	public static void main(String[] args) {
 		SpringApplication.run(ClientsNotificationApplication.class, args);
 	}
 
-	@Autowired
+	/*@Autowired
 	public void setCredConf(){
 		credConf.setDbName(System.getenv("db_name"));
 		credConf.setDbPass(System.getenv("db_pass"));
@@ -26,5 +22,5 @@ public class ClientsNotificationApplication {
 
 		credConf.setAtKey(System.getenv("at_key"));
 		credConf.setAtName(System.getenv("db_name"));
-	}
+	}*/
 }
